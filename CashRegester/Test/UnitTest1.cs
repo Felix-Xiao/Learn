2bc46b85bbddb12a225c669364e1584d2eb92f19
Felix Xiao <914321664@qq.com>
2016-07-08 17:11:17 +0800

    Test

---
+++ CashRegester/Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 1;
+            int b = 1;
+            Assert.IsTrue(a.Equals(b));
+        }
+    }
+}
