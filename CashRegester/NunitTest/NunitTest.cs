da7a061ec2c86257d6ca40db9b9ed885a4cec380
Felix Xiao <914321664@qq.com>
2016-07-11 16:20:55 +0800

    Nunit

---
+++ CashRegester/NunitTest/NunitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NunitTest
+{
+    [TestFixture()]
+    public class NunitTest
+    {
+        [Test()]
+        public void TestMethod1()
+        {
+        }
+    }
+}
